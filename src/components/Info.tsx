57298d36d667463f32ba45c0dda668bcdeda627f
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-10-08 20:26:41 -0300

    Criado componente Info

---
+++ src/components/Info.tsx
@@ -0,0 +1,17 @@
+import styles from './Info.module.css'
+
+export function Info() {
+
+    return (
+        <section className={styles.info}>
+            <p className={styles.tasksCreated}>
+                Tarefas criadas
+                <span className={styles.counter}>0</span>
+            </p>
+            <p className={styles.tasksCompleted}>
+                Concluídas
+                <span className={styles.counter}>0</span>
+            </p>
+        </section>
+    )
+}
