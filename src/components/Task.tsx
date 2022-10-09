a52888609e7970e94cb374efc9ab2e60cdb84fb8
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-10-09 14:41:55 -0300

    Adicionado checkbox do radix nas tasks e criado seus estilos

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,17 +1,39 @@
 import styles from './Task.module.css'
 
-import { Trash } from 'phosphor-react'
+import clsx from 'clsx'
+
+import { useState } from 'react'
+
+import { Trash, Check } from 'phosphor-react'
+
+import * as Checkbox from '@radix-ui/react-checkbox'
 
 export function Task() {
 
+    const [checked, setCheck] = useState(false)
+
+    function handleCheck() {
+        setCheck(!checked)
+    }
+
     return (
         <div className={styles.task}>
-            <div>
-                <p>
-                    Passear com as cachorras
-                </p>
-            </div>
-            <Trash size={24} className={styles.trash} />
+            <Checkbox.Root
+                checked={checked}
+                className={clsx(styles.checkbox,
+                    { [styles.checkboxChecked]: checked },
+                    { [styles.checkboxUnchecked]: !checked }
+                )}
+                onCheckedChange={handleCheck}
+            >
+                <Checkbox.Indicator className={styles.checkboxIndicator}>
+                    <Check size={12} weight="bold" />
+                </Checkbox.Indicator>
+            </Checkbox.Root>
+            <p>
+                Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.
+            </p>
+            <Trash className={styles.trash} />
         </div>
     )
 }
